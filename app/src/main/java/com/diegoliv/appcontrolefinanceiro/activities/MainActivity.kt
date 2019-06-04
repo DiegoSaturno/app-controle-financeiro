8b0c1014ccee0aa67fc2dcb34a9ee812998933b7
Diego Oliveira <diegosetubal10@hotmail.com>
2019-06-04 11:45:34 -0300

    Signup activity

--- app/src/main/java/com/diegoliv/appcontrolefinanceiro/activities/MainActivity.kt
+++ app/src/main/java/com/diegoliv/appcontrolefinanceiro/activities/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.diegoliv.appcontrolefinanceiro.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.diegoliv.appcontrolefinanceiro.R
+import com.diegoliv.appcontrolefinanceiro.activities.auth.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        val button = findViewById<Button>(R.id.button2)
+
+        button.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@MainActivity, SignUpActivity::class.java)
+
+            startActivity(intent)
+        })
     }
 }
--- app/src/main/java/com/diegoliv/appcontrolefinanceiro/activities/auth/SignUpActivity.kt
+++ app/src/main/java/com/diegoliv/appcontrolefinanceiro/activities/auth/SignUpActivity.kt
@@ -2,19 +2,48 @@ package com.diegoliv.appcontrolefinanceiro.activities.auth
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import com.diegoliv.appcontrolefinanceiro.R
 import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var nameTextView: TextView
+    private lateinit var lastNameTextView: TextView
+    private lateinit var emailTextView: TextView
+    private lateinit var passwordTextView: TextView
+    private lateinit var signUpButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        signUpButton = findViewById<Button>(R.id.button)
+        nameTextView = findViewById<TextView>(R.id.firstNameTextView)
+        lastNameTextView = findViewById<TextView>(R.id.lastNameTextView)
+        emailTextView = findViewById<TextView>(R.id.emailTextView)
+        passwordTextView = findViewById(R.id.passwordTextView)
+
         firebaseAuth = FirebaseAuth.getInstance()
+
+        setSignUpButtonOnClickListener()
     }
 
+    private fun setSignUpButtonOnClickListener() {
 
+        signUpButton.setOnClickListener(View.OnClickListener {
+
+        })
+    }
+
+    private fun createAccount(email: String, password: String) {
+
+    }
+
+    private fun validateForm() {
+
+    }
 }
